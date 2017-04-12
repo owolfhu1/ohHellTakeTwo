8ad6e85a0957469ead8c7d998fb493f8d907685b
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-04-12 18:34:03 -0400

    dd

--- server.js
+++ server.js
@@ -127,6 +127,13 @@ function deck() {
 
 function card(value, suit) {return [value, suit];}
 
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+
 function deal(id) {
 
     //io.sockets.emit('log', `<u>Starting new round</u>`);
