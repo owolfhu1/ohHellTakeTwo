68a878e6860baa5cbbf28af0cd1e3a2753370493
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-04-16 00:02:49 -0400

    .

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ const finishedGameArray = [];
 let emptyGame = function() {
     this.player1Id = null;
     this.player2Id = null;
-    this.round = 3;
+    this.round = 1;
     this.gameDeck = null;
     this.trump = null;
     this.inPlay = null;
-    this.plusMinus = -1;
+    this.plusMinus = 1;
     this.aceValue = 1;
 };
 
