0d408e0176e612a959595adb428fc8c319f279bd
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-05-06 00:37:26 -0400

    undo

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ io.on('connection', socket => {
         
         
         //test
-        
+        /*
         pg.connect(process.env.DATABASE_URL, function(err, client) {
             if (err) throw err;
             console.log('Connected to postgres! Getting schemas...');
@@ -95,7 +95,7 @@ io.on('connection', socket => {
                 //    console.log(JSON.stringify(row));
                 //});
         });
-        
+        */
         //end test
         
         if (login[USER_NAME] in passwordMap && !onlineNameArray.includes(login[USER_NAME])) {
