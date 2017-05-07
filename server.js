11845c7590bc8e2793a4e285e8166c33f6347916
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-05-06 21:01:21 -0400

    test

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (err) throw err;
     console.log('Connected to postgres! Getting schemas...');
     client
-        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .query('SELECT * FROM passbank;')
         .on('row', function(row) {
             console.log(JSON.stringify(row));
         });
@@ -691,4 +691,3 @@ const finishedGameMap = () => {
     }
     return map;
 };
-
